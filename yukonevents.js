a99f8fa39409b61bcb6a23f2bccc379187390481
Dino Rudniski <dinorudniski@gmail.com>
2013-08-01 21:42:05 -0700

    Added start and end dates

--- yukonevents.js
+++ yukonevents.js
@@ -8,20 +8,28 @@ Owners = Locations = new Meteor.Collection("Owners");
 Categories = new Meteor.Collection("Categories");
 Ads = new Meteor.Collection("Ads");
 Users = new Meteor.Collection("Users");
+
+
 if (Meteor.isClient) {
 	Template.yukonevents.happenings = function () {
 		return Occurs.find();
-	}
+	};
+
+	Template.eventsPanel.rendered = function(){
+		console.log("making date things");
+		$('#event-start').appendDtpicker();
+		$('#event-end').appendDtpicker();
+	};
 
 	Template.eventsPanel.events({
 		'click #submit-button': function () {
-			location_data = $('#event-location').val();
-			name = $('#event-name').val();
 			category = $('#event-category').val();
 			date = $('#event-date').val();
+			location_data = $('#event-location').val();
+			name = $('#event-name').val();
 			if (Meteor.user() != null) {
-				Occurs.insert({'name':name,'date':date, 'location':location_data});
 				uid = Meteor.userId();
+				Occurs.insert({'name':name,'date':date, 'location':location_data, 'uid':uid});
 				user = Users.find( { _id:uid } ).fetch();
 				console.log(user);
 				if (!user.length) {
